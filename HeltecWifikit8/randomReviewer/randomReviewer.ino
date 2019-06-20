a71ff947162910ed6f415b998c3bed54fb12fe00
mail@marcelpetrick.it <mail@marcelpetrick.it>
2019-06-20 20:36:42 +0200

    randomReviewer: print four candidates when pressing the "prg"-button (is GPIO0)

--- HeltecWifikit8/randomReviewer/randomReviewer.ino
+++ HeltecWifikit8/randomReviewer/randomReviewer.ino
@@ -10,17 +10,24 @@
 // use Board "Wemos D1 Mini R2", 80Mhz, 4 MB Flash, 921600baud to flash
 // COM10
 
+// display define
 U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(16);
+
+// button defines
+int bootButtonPin{0};
 
 void setup(void)
 {
+  pinMode(bootButtonPin, INPUT);
+
+  Serial.begin(115200);
+
   u8x8.begin();
 
   // if analog input pin 0 is unconnected, random analog
   // noise will cause the call to randomSeed() to generate
   // different seed numbers each time the sketch runs.
   // randomSeed() will then shuffle the random function.
-  Serial.begin(9600);
   randomSeed(analogRead(0));
 }
 
@@ -29,17 +36,45 @@ void setup(void)
 void loop(void)
 {
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  // clear all at once?
-  u8x8.clearLine(0);
-  u8x8.clearLine(1);
-  u8x8.clearLine(2);
-  
+  u8x8.clearDisplay();
+
   u8x8.drawString(0, 0, "Laku noc,");
   u8x8.drawString(0, 1, "Puska! <3");
 
-  u8x8.drawString(0, 2, getRandomReviewer().c_str());
-  
+  //u8x8.drawString(0, 2, getRandomReviewer().c_str());
+
   delay(666);
+
+  if (checkButtons())
+  {
+    u8x8.clearDisplay();
+    for (int i = 0; i < 4; i++)
+    {
+      //u8x8.clearLine(i);
+      u8x8.drawString(0, i, getRandomReviewer().c_str());
+    }
+    delay(3 * 666);
+  }
+}
+
+//----------------------------------------------------------------------------------------------------
+
+//! @brief Intermediate. Just to see if GPIO0 is really usable.
+//! @todo Replace this later with interrupt-driven events.
+bool checkButtons()
+{
+  Serial.println("checkButtons()");
+
+  bool returnValue(false);
+
+  //after booting the GPIO0 for BOOT is free for use
+  if (digitalRead(bootButtonPin) != HIGH)
+  {
+    Serial.println("bootButton pressed");
+    returnValue = true;
+  }
+
+  return returnValue;
 }
 
 //----------------------------------------------------------------------------------------------------
