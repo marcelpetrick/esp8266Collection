7736d7e37bbc079f2d3b7158b04fbeb89c2b60bd
mail@marcelpetrick.it <mail@marcelpetrick.it>
2019-06-20 19:44:09 +0200

    randomReviewer: added the basic random-functionality with printing

--- HeltecWifikit8/randomReviewer/randomReviewer.ino
+++ HeltecWifikit8/randomReviewer/randomReviewer.ino
@@ -1,3 +1,6 @@
+//! @brief randomReviewer: smol fun project to relieve the stress of finding a fitting reviewer
+//! @author mail@marcelpetrick.it
+
 #include <Arduino.h>
 #include <U8x8lib.h>
 
@@ -12,7 +15,16 @@ U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(16);
 void setup(void)
 {
   u8x8.begin();
+
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
 }
+
+//----------------------------------------------------------------------------------------------------
 
 void loop(void)
 {
@@ -20,7 +32,46 @@ void loop(void)
   u8x8.drawString(0, 0, "Laku noc,");
   delay(1000);
   u8x8.clearLine(0);
+
   u8x8.drawString(0, 2, "Puska! <3");
   delay(1000);
   u8x8.clearLine(2);
+
+  u8x8.drawString(0, 0, getRandomReviewer());
+  delay(1000);
+  u8x8.clearLine(0);
 }
+
+//----------------------------------------------------------------------------------------------------
+
+//! @todo improve this later by using a std::vector ..
+#define PEOPLEARRAYSIZE 7
+// @brief function to determine randomly inside a given array one of the values
+// @returns String like "MPE was picked".
+String getRandomReviewer()
+{
+  Serial.println("getRandomReviewer()");
+
+  // init the array of possible reviewers
+  String people[PEOPLEARRAYSIZE] = {
+    "GSC",
+    "MPE",
+    "NLE",
+    "RNI",
+    "HGA",
+    "MDR",
+    "NKU",
+  };
+
+  // randomly determine an index
+  int const randomIndex = random(PEOPLEARRAYSIZE); // exclusive the last value
+  Serial.println(randomIndex);
+
+  // create the result-string
+  String returnValue = people[randomIndex] + " is your guy! :)";
+
+  // and return it
+  return returnValue;
+}
+
+//----------------------------------------------------------------------------------------------------
